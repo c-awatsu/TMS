10f50a4fc5091a7b1d4f45f3f4eeead653b73dd4
a-murakamiiiiiii <cist.b213.a.murakami@gmail.com>
2016-01-15 19:31:43 +0900

    TestRecordService.getTestProgressの内容をfor文で処理するものに差し替え

--- src/main/java/jp/ac/chitose/tms/Service/TestRecordService.java
+++ src/main/java/jp/ac/chitose/tms/Service/TestRecordService.java
@@ -1,10 +1,7 @@
 package jp.ac.chitose.tms.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import jp.ac.chitose.tms.Bean.TestItem;
 import jp.ac.chitose.tms.Bean.TestProgressItem;
-import jp.ac.chitose.tms.Bean.TestRecordItem;
 import jp.ac.chitose.tms.Repositoy.ITestRecordReposiory;
 import jp.ac.chitose.tms.Repositoy.ITestRepository;
 import lombok.val;
@@ -19,18 +16,29 @@ public class TestRecordService implements ITestRecordService{
 	@Autowired
 	private ITestRepository tRepository;
 
+//	@Override
+//	public TestProgressItem getTestProgress(Integer productId) {
+//
+//		val testItems = tRepository.fetchTestItems(productId);
+//		int testItemsNum =  testItems.size();
+//		//TODO streamの返り値があっているか要チェック
+//		List<TestRecordItem> successTestItem =  testItems.stream()
+//				.map(i -> {
+//					return trRepository.selectLatestTestRecord(i.getTestId());
+//				}).filter(i->i.getResult())
+//				.collect(Collectors.toList());
+//		return new TestProgressItem(successTestItem.size(),testItemsNum);
+//
+//	}
+
 	@Override
 	public TestProgressItem getTestProgress(Integer productId) {
-
+		//TODO streamやだー！
+		int count = 0;
 		val testItems = tRepository.fetchTestItems(productId);
-		int testItemsNum =  testItems.size();
-		//TODO streamの返り値があっているか要チェック
-		List<TestRecordItem> successTestItem =  testItems.stream()
-				.map(i -> {
-					return trRepository.selectLatestTestRecord(i.getTestId());
-				}).filter(i->i.getResult())
-				.collect(Collectors.toList());
-		return new TestProgressItem(successTestItem.size(),testItemsNum);
-
+		for(TestItem testItem: testItems){
+			if(trRepository.selectLatestTestRecord(testItem.getTestId()).getResult())count++;
+		}
+		return new TestProgressItem(count, testItems.size());
 	}
 }
