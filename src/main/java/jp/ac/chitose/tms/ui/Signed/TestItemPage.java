9d768a8627448735fa4121b67fd831025633f01b
a-murakamiiiiiii <cist.b213.a.murakami@gmail.com>
2016-01-19 14:45:27 +0900

    テストの個別ページを途中まで作った

--- src/main/java/jp/ac/chitose/tms/ui/Signed/TestItemPage.java
+++ src/main/java/jp/ac/chitose/tms/ui/Signed/TestItemPage.java
@@ -1,12 +1,22 @@
 package jp.ac.chitose.tms.ui.Signed;
 
+import jp.ac.chitose.tms.Bean.TestItem;
+import jp.ac.chitose.tms.Bean.TestRecordItem;
 import jp.ac.chitose.tms.Service.ITestRecordService;
 import jp.ac.chitose.tms.Service.ITestService;
 import lombok.val;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.PropertyListView;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.util.ListModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.wicketstuff.annotation.mount.MountPath;
 
+@MountPath("/test")
 public class TestItemPage extends WebPage {
 
 	@SpringBean
@@ -18,6 +28,25 @@ public class TestItemPage extends WebPage {
 	public TestItemPage(){
 		int testId = 1;
 
-		val testItem = testService;//Service
+		val testItem = new WebMarkupContainer("testItem", new CompoundPropertyModel<TestItem>(testService.fetchTestItem(testId)));
+		testItem.add(new Label("testId"));
+		testItem.add(new Label("classification"));
+		testItem.add(new Label("step"));
+		testItem.add(new Label("expectedOutput"));
+		add(testItem);
+
+		val RecordListModel = new ListModel<TestRecordItem>(null);//TODO TestRecordのListを入れる
+		val testRecordList = new PropertyListView<TestRecordItem>("", RecordListModel){
+			@Override
+			protected void populateItem(ListItem<TestRecordItem> item) {
+				item.add(new Label("testRecordId"));
+				item.add(new Label("testDate"));
+				item.add(new Label("testerId"));
+				item.add(new Label("result"));
+				item.add(new Label("note"));
+			}
+		};
+		add(testRecordList);
+
 	}
 }
