3e10ccefc05d459e279e51eaa94a6483617fe234
a-murakamiiiiiii <cist.b213.a.murakami@gmail.com>
2016-01-19 17:14:12 +0900

    テストの個別ページが見た目以外は大体完成。セッションの使い方わからん

--- src/main/java/jp/ac/chitose/tms/ui/Signed/TestItemPage.java
+++ src/main/java/jp/ac/chitose/tms/ui/Signed/TestItemPage.java
@@ -1,5 +1,7 @@
 package jp.ac.chitose.tms.ui.Signed;
 
+import java.sql.Date;
+
 import jp.ac.chitose.tms.Bean.TestItem;
 import jp.ac.chitose.tms.Bean.TestRecordItem;
 import jp.ac.chitose.tms.Service.ISignService;
@@ -7,9 +9,13 @@ import jp.ac.chitose.tms.Service.ITestRecordService;
 import jp.ac.chitose.tms.Service.ITestService;
 import lombok.val;
 
+import org.apache.wicket.extensions.markup.html.form.DateTextField;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.RequiredTextField;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -29,28 +35,58 @@ public class TestItemPage extends WebPage {
 	@SpringBean
 	private ITestRecordService testRecordService;
 
-	public TestItemPage(){
-		int testId = 1;
+	// テスト用コンストラクタ あとでけす
+	public TestItemPage() {
+		this(1);
+	}
 
-		val testItem = new WebMarkupContainer("testItem", new CompoundPropertyModel<TestItem>(testService.fetchTestItem(testId)));
+	public TestItemPage(final int testId) {
+
+		val testItem = new WebMarkupContainer("testItem",
+				new CompoundPropertyModel<TestItem>(
+						testService.fetchTestItem(testId)));
 		testItem.add(new Label("testId"));
 		testItem.add(new Label("classification"));
 		testItem.add(new Label("step"));
 		testItem.add(new Label("expectedOutput"));
 		add(testItem);
 
-		val RecordListModel = new ListModel<TestRecordItem>(testRecordService.fetchTestRecordItems(testId));
-		val testRecordList = new PropertyListView<TestRecordItem>("testRecordList", RecordListModel){
+		val testRecordList = new PropertyListView<TestRecordItem>(
+				"testRecordList", new ListModel<TestRecordItem>(
+						testRecordService.fetchTestRecordItems(testId))) {
 			@Override
 			protected void populateItem(ListItem<TestRecordItem> item) {
 				val testRecordItem = item.getModelObject();
 				item.add(new Label("testRecordId"));
 				item.add(new Label("testDate"));
-				item.add(new Label("testerName", signService.fetchNickName(testRecordItem.getTesterId())));
-				item.add(new Label("result", testRecordItem.getResult() ? "○":"×"));
+				item.add(new Label("testerName", signService
+						.fetchNickName(testRecordItem.getTesterId())));
+				item.add(new Label("result", testRecordItem.getResult() ? "○"
+						: "×"));
 				item.add(new Label("note"));
 			}
 		};
 		add(testRecordList);
+
+		val testRecordInputForm = new Form<TestRecordItem>(
+				"testRecordInputForm",
+				new CompoundPropertyModel<TestRecordItem>(new TestRecordItem(0,
+						new Date(0), 1, false, "", testId))) {//TODO testerIdをセッションから取ってくる
+			@Override
+			protected void onSubmit() {
+				super.onSubmit();
+				val testRecordItem = getModelObject();
+				System.out.println("あおおおうういいえええあ"
+						+ testRecordItem.getTestDate().toString());
+			}
+		};
+		testRecordInputForm.add(new DateTextField("testDate", "yyyy-mm-dd"));
+		testRecordInputForm.add(new Label("testerName", signService
+				.fetchNickName(testRecordInputForm.getModelObject()
+						.getTesterId())));
+		testRecordInputForm.add(new CheckBox("result"));
+		testRecordInputForm.add(new RequiredTextField<String>("note"));
+		add(testRecordInputForm);
+
 	}
 }
